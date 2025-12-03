f38bc8020bf53411532f83c27feca05c1afa971d
Vitaly-Trepalin <vitalietrepalin@yandex.ru>
2025-12-03 12:08:21 +0300

    finalize schema.sql v1.2

--- commerce/shopping-cart/src/main/resources/schema.sql
+++ commerce/shopping-cart/src/main/resources/schema.sql
@@ -10,5 +10,8 @@ CREATE TABLE IF NOT EXISTS cart_schema.product_quantity (
     id               BIGSERIAL    PRIMARY KEY,
     product_id       VARCHAR(255) NOT NULL,
     quantity         BIGINT       NOT NULL,
-    shopping_cart_id VARCHAR(255) NOT NULL REFERENCES shopping_cart(shopping_cart_id) ON DELETE CASCADE
+    shopping_cart_id VARCHAR(255) NOT NULL,
+        FOREIGN KEY (shopping_cart_id)
+            REFERENCES cart_schema.shopping_cart(shopping_cart_id)
+            ON DELETE CASCADE
 );
